ba420292b27bd618456821c34972d0c87190f7ab
eman-NEMO <emanelkaser@gmail.com>
2022-12-01 00:14:13 +0200

    new

---
+++ Java Code/src/main/java/com/javacode/UserFunctionallity/checkDiscount/Check.java
@@ -0,0 +1,9 @@
+package com.javacode.UserFunctionallity.checkDiscount;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public interface Check {
+    public ArrayList check(Map m);
+}
