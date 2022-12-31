3d9c00bda13d6dea7dac6c20f8f6649c9339ca5f
Sooma-M <sooma3mohammed@gmail.com>
2022-12-31 17:42:51 +0200

    Update Services

--- Phase 2/PaymentApplication/src/main/java/com/PaymentApplication/ServiceProvider/ServiceHandler.java
+++ Phase 2/PaymentApplication/src/main/java/com/PaymentApplication/ServiceProvider/ServiceHandler.java
@@ -7,9 +7,12 @@ import java.util.HashMap;
 public class ServiceHandler implements IServiceHandler{
     @Override
     public void saveService(String type, String name, HashMap m) {
-        ServiceProvider serviceProvider = ServiceFactory.createFactory(type).createService(name);
+        ServiceProviderFactory ff = ServiceFactory.createFactory(type);
+        if (ff == null)
+            throw new IllegalArgumentException("Enter correct provider type");
+        ServiceProvider serviceProvider = ff.createService(name);
         if (serviceProvider == null)
-            throw new IllegalArgumentException("Enter correct provider type and name");
+            throw new IllegalArgumentException("Enter correct provider name");
         CurrentUser.checkUser();
         serviceProvider.setParameters(m);
         CurrentService.setService(serviceProvider);
