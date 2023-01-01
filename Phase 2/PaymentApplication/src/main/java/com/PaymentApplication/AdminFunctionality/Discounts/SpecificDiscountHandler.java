a507433feb0645b911d7bb731f6da2fec96dcc84
Sooma-M <sooma3mohammed@gmail.com>
2023-01-01 02:33:06 +0200

    Update specific discount

--- Phase 2/PaymentApplication/src/main/java/com/PaymentApplication/AdminFunctionality/Discounts/SpecificDiscountHandler.java
+++ Phase 2/PaymentApplication/src/main/java/com/PaymentApplication/AdminFunctionality/Discounts/SpecificDiscountHandler.java
@@ -3,6 +3,7 @@ package com.PaymentApplication.AdminFunctionality.Discounts;
 
 import com.PaymentApplication.ServiceProvider.ServiceFactory;
 import com.PaymentApplication.ServiceProvider.ServiceProvider;
+import com.PaymentApplication.ServiceProvider.ServiceProviderFactory;
 import com.PaymentApplication.User.CurrentUser;
 
 import java.util.HashMap;
@@ -11,8 +12,12 @@ public class SpecificDiscountHandler implements DiscountHandler {
     @Override
     public void execute(HashMap m) {
         CurrentUser.checkAdmin();
-        ServiceProvider service = ServiceFactory.createFactory((String) m.get("service-name"))
-                .createService((String) m.get("service-name"));
+        ServiceProviderFactory ff = ServiceFactory.createFactory((String) m.get("service-name"));
+        if (ff == null)
+            throw new IllegalArgumentException("enter correct service name!!!");
+        ServiceProvider service = ff.createService((String) m.get("service-name"));
+        if (service == null)
+            throw new IllegalArgumentException("enter correct service name!!!");
         Discount discount = DiscountFactory.makeDiscount(m);
         service.addDiscount(discount);
     }
